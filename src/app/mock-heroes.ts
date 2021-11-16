1f4162c45ca2376c36549989f9895109b9a89a9b
Nigmatova Damel <damelya_13524@mail.ru>
2021-11-16 13:43:45 -0800

    added hero services

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+export const HEROES: Hero[] = [
+  {id:1, name:'Batman'},
+  {id:2, name:'Spiderman'},
+  {id:3, name:'Flash'},
+  {id:4, name:'Aquaman'},
+  {id:5, name:'Superman'},
+]
